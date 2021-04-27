c311fba5f84793f5b56c6e15d7930fecc262cb19
Braiden <bmxfiend1@gmail.com>
2021-04-27 11:48:56 -0400

    Add files via upload

---
+++ C_Interfacing/Tutorial/CSourceCode/source.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void hello()
+{
+    printf("HELLO FROM C");
+}
+
---
+++ C_Interfacing/Tutorial/CSourceCode/source.h
@@ -0,0 +1,13 @@
+/**
+ * @file source.h
+ * @author Braiden Gole
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-27
+ * @copyright Copyright (c) Braiden Gole 2021
+ */
+#ifndef SOURCE
+#define SOURCE "source.h"
+
+void hello();
+#endif
